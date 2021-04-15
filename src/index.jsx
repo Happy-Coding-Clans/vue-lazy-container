bf767464dba7db230383e8150c0158008c0c0fdf
huangshuwei <hsw89520@163.com>
2021-04-15 11:49:40 +0800

    add intersectionOption

--- src/index.jsx
+++ src/index.jsx
@@ -7,6 +7,16 @@ export default {
         tagName: {
             type: String,
             required: true
+        },
+        // IntersectionObserver option
+        intersectionOption: {
+            type: Object,
+            default: null
+            /*
+            root: document.querySelector("#scrollArea"),
+            rootMargin: "0px",
+            threshold: 1.0
+            */
         }
     },
     data() {
@@ -14,33 +24,44 @@ export default {
             observer: null
         };
     },
+    watch: {
+        // intersectionOption watch
+        intersectionOption: {
+            handler: function() {
+                this.destroyObserver();
+                this.createObserver();
+            }
+        }
+    },
     methods: {
-        init() {
+        // create observer
+        createObserver() {
             const callback = (entries, observer) => {
                 entries.forEach(entry => {
                     this.$emit("change", entry, observer);
                 });
             };
 
-            const options = {
-                /* root: document.querySelector("#scrollArea"),
-                rootMargin: "0px",
-                threshold: 1.0 */
-            };
+            const options = this.intersectionOption;
 
             this.observer = new IntersectionObserver(callback, options);
 
             this.observer.observe(this.$el);
+        },
+
+        // destroy observer
+        destroyObserver() {
+            if (this.observer) {
+                this.observer.disconnect();
+                this.observer = null;
+            }
         }
     },
     mounted() {
-        this.init();
+        this.createObserver();
     },
     beforeDestroy() {
-        if (this.observer) {
-            this.observer.disconnect();
-            this.observer = null;
-        }
+        this.destroyObserver();
     },
     render() {
         return <this.tagName>{this.$slots.default}</this.tagName>;
