0b75806e18216a13fea6533e44786e6924d8f770
huangshuwei <hsw89520@163.com>
2021-04-19 22:21:25 +0800

    router switch back to top

--- examples/src/router/index.js
+++ examples/src/router/index.js
@@ -26,7 +26,10 @@ const routes = [
 ];
 
 const router = new VueRouter({
-  routes
+  routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
 });
 
 export default router;
