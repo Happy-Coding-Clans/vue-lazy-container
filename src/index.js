f20478f5df84418b38a4d57ef8596ae8da339230
huangshuwei <hsw89520@163.com>
2021-04-20 23:43:17 +0800

    init gh-pages

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import VueLazyContainer from "./index.jsx";
-
-VueLazyContainer.install = function(Vue) {
-    Vue.component(VueLazyContainer.name, VueLazyContainer);
-};
-
-export default VueLazyContainer;
